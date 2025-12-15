79f0443875bd6dffb65b9f60e2d3ad6fb80ad80d
Brett Longworth <blongworth@whoi.edu>
2025-12-15 14:09:36 -0500

    add matlab flux processing scripts

---
+++ matlab/README.txt
@@ -0,0 +1,11 @@
+This directory contains GEMS data converted
+to input files for flux calculation in MATLAB. The input files are periods
+between lander repositioning and have had intervals shorter than 15 minutes
+removed. 
+It also contains the MATLAB flux script, set up for batch input,
+a shell script to coordinate running all input files, and output
+files containing fluxes and cumulative fluxes.
+
+rotation angles used in the MATLAB script were calculated in
+the R script bin_timeseries.R. Phi angles were entered manually
+into the MATLAB script.
---
+++ matlab/run_eddyflux.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Default range
+start=1
+end=6
+
+# Check for optional arguments
+if [[ $# -eq 2 ]]; then
+  start=$1
+  end=$2
+elif [[ $# -eq 1 ]]; then
+  cd $1
+elif [[ $# -ne 0 ]]; then
+  echo "Usage: $0 [start] [end]"
+  echo "       start and end are optional and must both be provided to define the range."
+  exit 1
+fi
+
+# Set the base name for the files
+data_prefix="lecs_ml_data_"
+timestamp_prefix="lecs_ml_timestamps_"
+file_extension=".dat"
+
+# Loop through numbers 1 to 15
+# parallelized using GNU parallel
+seq $start $end | parallel --eta "
+data_file='${data_prefix}{}${file_extension}'
+timestamp_file='${timestamp_prefix}{}${file_extension}'
+
+if [[ -f \"\$data_file\" && -f \"\$timestamp_file\" ]]; then
+    echo \"Processing files: \$data_file and \$timestamp_file with rotation {}\"
+    matlab -nodisplay -nosplash -r \"eddyflux_batch('\$data_file', '\$timestamp_file', {}); exit;\"
+else
+    echo \"Files \$data_file or \$timestamp_file not found. Skipping.\"
+fi
+"
